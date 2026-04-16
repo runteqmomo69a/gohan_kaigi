2993531d6cdec7e0d9884190a569654a77d527ae
Momo <uan2299.ars113@ezweb.ne.jp>
2026-04-17 03:32:12 +0900

    feat: 主要画面の背景トーンを統一

--- app/helpers/shop_logs_helper.rb
+++ app/helpers/shop_logs_helper.rb
@@ -2,31 +2,18 @@
 
 module ShopLogsHelper
   CATEGORY_COLORS = [
-    "bg-blue-100 text-blue-700",
-    "bg-green-100 text-green-700",
-    "bg-purple-100 text-purple-700",
-    "bg-pink-100 text-pink-700",
-    "bg-yellow-100 text-yellow-700",
-    "bg-indigo-100 text-indigo-700",
-    "bg-red-100 text-red-700",
-    "bg-teal-100 text-teal-700",
-    "bg-orange-100 text-orange-700",
-    "bg-cyan-100 text-cyan-700",
-
-    "bg-blue-50 text-blue-600",
-    "bg-green-50 text-green-600",
-    "bg-purple-50 text-purple-600",
-    "bg-pink-50 text-pink-600",
-    "bg-yellow-50 text-yellow-600",
-    "bg-indigo-50 text-indigo-600",
-    "bg-red-50 text-red-600",
-    "bg-teal-50 text-teal-600",
-    "bg-orange-50 text-orange-600",
-    "bg-cyan-50 text-cyan-600"
+    "bg-[#fff1eb] text-[#c45f3d]",
+    "bg-[#fff6dc] text-[#b07a10]",
+    "bg-[#eef5ec] text-[#5f7d4e]",
+    "bg-[#f3eee8] text-[#7a6757]",
+    "bg-[#f8ece7] text-[#b1654e]",
+    "bg-[#f2efe6] text-[#7b7467]",
+    "bg-[#f7f0dc] text-[#8f6f16]",
+    "bg-[#edf3f2] text-[#52736b]"
   ].freeze
 
   def category_color(category)
-    return "bg-gray-100 text-gray-700" if category.blank?
+    return "bg-[#fff1eb] text-[#c45f3d]" if category.blank?
 
     index = category.hash % CATEGORY_COLORS.size
     CATEGORY_COLORS[index]
