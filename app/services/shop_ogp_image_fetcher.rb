54f3e7b7d6c2162e37cb5c9bfbdae336a09cce0f
Momo <uan2299.ars113@ezweb.ne.jp>
2026-04-28 15:13:31 +0900

    chore: Instagram取得調査用のログを追加する
    - ShopNameFetcherに一時ログを追加
    - ShopOgpImageFetcherに一時ログを追加
    - 本番でInstagramレスポンスを確認できるようにする

--- app/services/shop_ogp_image_fetcher.rb
+++ app/services/shop_ogp_image_fetcher.rb
@@ -13,6 +13,7 @@ class ShopOgpImageFetcher
     return Result.new(image_url: nil, error: I18n.t("services.shop_ogp_image_fetcher.blank_url")) if @url.blank?
 
     response = Faraday.get(@url)
+    log_instagram_response(response.body, response.status) if instagram_url?
     return Result.new(image_url: nil, error: I18n.t("services.shop_ogp_image_fetcher.request_failed")) unless response.success?
 
     image_url = extract_image_url(response.body)
@@ -35,5 +36,26 @@ class ShopOgpImageFetcher
     URI.join(@url, raw_image_url).to_s
   rescue URI::InvalidURIError
     nil
+  end
+
+  def instagram_url?
+    uri = URI.parse(@url)
+    uri.host.to_s.downcase.include?("instagram.com")
+  rescue URI::InvalidURIError
+    false
+  end
+
+  def log_instagram_response(body, status)
+    document = Nokogiri::HTML(body)
+
+    Rails.logger.info(
+      "[Instagram ShopOgpImageFetcher] " \
+      "url=#{@url} " \
+      "status=#{status} " \
+      "title=#{document.at_css("title")&.text&.squish.inspect} " \
+      "og_title=#{document.at_css('meta[property=\"og:title\"]')&.[]("content")&.squish.inspect} " \
+      "og_image=#{document.at_css('meta[property=\"og:image\"]')&.[]("content")&.squish.inspect} " \
+      "body_head=#{body.to_s.first(500).inspect}"
+    )
   end
 end
