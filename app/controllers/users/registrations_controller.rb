55cb5b0e0d9071e97a035217f4162dd92f9f4b3b
Momo <uan2299.ars113@ezweb.ne.jp>
2026-04-30 02:40:25 +0900

    chore: 未公開のアカウント設定機能を閉じる
    - registrations edit/update/destroy を本リリース中は無効化
    - 直URLアクセス時は dashboard へ戻す
    - 未公開用の alert 文言を追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :redirect_if_authenticated, only: %i[new]
+  before_action :redirect_unreleased_account_settings, only: %i[edit update destroy]
 
   private
 
@@ -7,5 +8,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
     return unless user_signed_in?
 
     redirect_to dashboard_path, alert: t("flash.devise.already_authenticated.alert")
+  end
+
+  def redirect_unreleased_account_settings
+    redirect_to dashboard_path, alert: t("flash.devise.account_settings_unavailable.alert")
   end
 end
