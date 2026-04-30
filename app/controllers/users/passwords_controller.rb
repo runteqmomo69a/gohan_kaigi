93b79ea4359af048714e3872fabe3aaa85cfdb29
Momo <uan2299.ars113@ezweb.ne.jp>
2026-04-30 16:07:59 +0900

    fix: 本リリース向けにパスワード再設定導線を閉じる
    - ログイン画面のパスワード再設定リンクを削除
    - passwords controller で直URLアクセスをログイン画面へ戻す
    - 本リリース後公開予定の alert 文言を追加

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,5 +1,6 @@
 class Users::PasswordsController < Devise::PasswordsController
   before_action :redirect_if_authenticated, only: %i[new edit]
+  before_action :redirect_unreleased_password_reset, only: %i[new create edit update]
 
   private
 
@@ -7,5 +8,10 @@ class Users::PasswordsController < Devise::PasswordsController
     return unless user_signed_in?
 
     redirect_to dashboard_path, alert: t("flash.devise.already_authenticated.alert")
+  end
+
+  def redirect_unreleased_password_reset
+    redirect_to new_user_session_path,
+      alert: t("flash.devise.password_reset_unavailable.alert")
   end
 end
